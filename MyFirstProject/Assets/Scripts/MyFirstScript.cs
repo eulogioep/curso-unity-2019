d7e5a401dc5fbe112a752d8cccfd007cb15868c8
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2020-06-14 14:42:43 +0200

    Visto clases y estructuras

--- MyFirstProject/Assets/Scripts/MyFirstScript.cs
+++ MyFirstProject/Assets/Scripts/MyFirstScript.cs
@@ -8,17 +8,35 @@ public class MyFirstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Clase
         Character hero = new Character();//Creo una instancia de la clase Character. Constructor por Defecto
-
         Character heroine = new Character("Lara Croft");//Constructor personalizado
-
         Character archer = new Character("Ojo de Alcón", 3);//Uso del tercer constructor
 
+        //Estrucutura
+        Weapon sword = new Weapon("Espada roma", 5);
+        Weapon sword2 = sword;
 
-        //Debug.LogFormat("Hero: {0} - {1} EXP", hero.name, hero.exp);
-        //Debug.LogFormat("Heroina: {0} - {1} EXP", heroine.name, heroine.exp);
-        //Debug.LogFormat("Hero: {0} - {1} EXP", archer.name, archer.exp);
+        //Character es una clase con la variable que se crea apunta a un puntero de memoria.
+        //hero2 y hero apuntan al mismo puntero. Si se modifica una, se cambia el otro.
+        Character hero2 = hero;
+        hero.PrintCharacterStats();
+        hero2.PrintCharacterStats();
 
+        hero.name = "Madonna";
+        hero2.PrintCharacterStats();
+        Debug.Log("*****************");
+
+        //Prueba con estructura
+        sword2.PrintWeaponStats();
+        sword2.name = "Excalibur";
+        sword2.damage = 255;
+
+        sword.PrintWeaponStats();//Espada roma - 5 daño
+        sword2.PrintWeaponStats();//Excalibur - 255 daño
+
+
+        Debug.Log("*****************");
         hero.PrintCharacterStats();
         heroine.PrintCharacterStats();
         archer.PrintCharacterStats();
---
+++ MyFirstProject/Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Weapon
+{
+    //Variables. No se inicializan por ser una estructura
+    public string name;
+    public int damage;
+
+    //Constructor. Único, público, se llamará igual que la estructura y tomará todas las variables
+    public Weapon(string name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    //Las estructuras pueden tener métodos
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Arma: {0} - {1} daño", this.name, this.damage);
+    }
+}
